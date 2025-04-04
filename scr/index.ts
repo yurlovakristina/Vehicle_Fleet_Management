c9431d1552e4c9c26190eb92356bab37a7006574
yurlovakristina <yurlovakristina9@gmail.com>
2025-04-05 00:58:44 +0200

    Update index.ts

--- scr/index.ts
+++ scr/index.ts
@@ -3,23 +3,28 @@ import { Truck } from "./Truck";
 import { Motorcycle } from "./Motorcycle";
 import { Garage } from "./Garage";
 
-// Создаем гараж
+
+// Create a garage
 const myGarage = new Garage();
 
-// Добавляем машины
+
+// Add cars
 const myCar = new Car("Toyota", "Camry", 2022, 5);
 const myTruck = new Truck("Volvo", "FH16", 2019, 20000);
 const myBike = new Motorcycle("Harley-Davidson", "Iron 883", 2021, false);
 
-// Запускаем двигатели
+
+// Start the engines
 myCar.startEngine();
 myTruck.startEngine();
 myBike.startEngine();
 
-// Добавляем в гараж
+
+// Add to garage
 myGarage.addVehicle(myCar);
 myGarage.addVehicle(myTruck);
 myGarage.addVehicle(myBike);
 
-// Выводим список транспорта в гараже
+
+// Display a list of vehicles in the garage
 myGarage.listVehicles();
